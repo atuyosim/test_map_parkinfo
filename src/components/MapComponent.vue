1179d84d132cdb2c19df041ded8d977cd5dcdca4
amaekawa <atsuyoshi.maekawa@mkyosho.co.jp>
2025-01-12 09:41:57 +0900

    1/12 9:42

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -1,56 +1,71 @@
 <template>
-  <div id="map"></div>
+  <div ref="mapContainer" class="map-container"></div>
 </template>
 
 <script>
 export default {
-  props: ["parkingData"],
-  watch: {
-    parkingData: "updateMapMarkers",
-  },
-  methods: {
-    initMap() {
-      this.map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 35.6895, lng: 139.6917 },
-        zoom: 10,
-      });
-    },
-    updateMapMarkers() {
-      this.markers.forEach((marker) => marker.setMap(null));
-      this.markers = this.parkingData.map((parking) => {
-        const iconPath =
-          parking.company === 1
-            ? "/res/pin_times.png"
-            : "/res/pin_meitetsukyosho.png";
-        const marker = new google.maps.Marker({
-          position: { lat: parking.latitude, lng: parking.longitude },
-          map: this.map,
-          icon: iconPath,
-          animation: google.maps.Animation.DROP,
-        });
-        marker.addListener("click", () => {
-          this.$emit("markerClicked", parking);
-        });
-        return marker;
-      });
-    },
-  },
-  mounted() {
-    this.initMap();
-    this.updateMapMarkers();
-  },
+  name: "MapComponent",
   data() {
     return {
-      map: null,
-      markers: [],
+      map: null, // Google Maps オブジェクトを保持
     };
+  },
+  methods: {
+    // Google Maps API を動的にロードする関数
+    loadGoogleMapsAPI() {
+      return new Promise((resolve, reject) => {
+        // Google Maps API がすでにロードされている場合は再ロードしない
+        if (typeof google !== "undefined") {
+          resolve();
+          return;
+        }
+
+        // スクリプトタグを作成して API をロード
+        const script = document.createElement("script");
+        script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.VUE_APP_GOOGLE_MAPS_API_KEY}&callback=initMap`;
+        script.async = true;
+        script.defer = true;
+
+        // スクリプトのロードが完了したら resolve
+        script.onload = resolve;
+
+        // スクリプトのロードエラー時に reject
+        script.onerror = () => reject(new Error("Failed to load Google Maps API"));
+
+        document.head.appendChild(script);
+      });
+    },
+    // Google Maps を初期化する関数
+    initMap() {
+      const mapOptions = {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+      };
+
+      // マップを指定の要素に描画
+      this.map = new google.maps.Map(this.$refs.mapContainer, mapOptions);
+    },
+  },
+  async mounted() {
+    // グローバルスコープにコールバック関数を登録
+    window.initMap = this.initMap;
+
+    try {
+      // Google Maps API をロード
+      await this.loadGoogleMapsAPI();
+
+      // マップを初期化
+      this.initMap();
+    } catch (error) {
+      console.error(error.message);
+    }
   },
 };
 </script>
 
 <style>
-#map {
-  height: 100%;
+.map-container {
   width: 100%;
+  height: 400px;
 }
 </style>
